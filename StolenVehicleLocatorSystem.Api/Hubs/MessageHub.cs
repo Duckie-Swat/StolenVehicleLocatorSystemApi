3236ed16a1431263ca8e393d7cc03318fe485da9
Hung Dinh Xuan <hungdinhxdev@gmail.com>
2022-08-25 13:31:28 +0700

    #28 Setup signalR

---
+++ StolenVehicleLocatorSystem.Api/Hubs/MessageHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using StolenVehicleLocatorSystem.Api.Hubs.Payloads;
+
+namespace StolenVehicleLocatorSystem.Api.Hubs
+{
+    
+    [Authorize]
+    public class MessageHub : Hub
+    {
+        public async Task Send(MessagePayload message)
+        {
+            await Clients.User(message.To!).SendAsync("SendMessage", message);
+        }
+    }
+}
---
+++ StolenVehicleLocatorSystem.Api/Hubs/Providers/EmailBasedUserIdProvider.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Security.Claims;
+
+namespace StolenVehicleLocatorSystem.Api.Hubs.Providers
+{
+    public class EmailBasedUserIdProvider : IUserIdProvider
+    {
+        public virtual string GetUserId(HubConnectionContext connection)
+        {
+            return connection.User?.FindFirst(ClaimTypes.Email)?.Value!;
+        }
+    }
+}
